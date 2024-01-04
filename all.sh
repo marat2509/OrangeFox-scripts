80b48811441ccdad2c2da5eb5e8909ad6bc24b07
marat2509 <93652988+marat2509@users.noreply.github.com>
2024-01-04 22:57:36 +0300

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo test
+sudo apt update
 sudo apt install curl -y
 curl -fSsl https://raw.githubusercontent.com/marat2509/OrangeFox-scripts/main/prepare.sh | bash
 curl -fSsl https://raw.githubusercontent.com/marat2509/OrangeFox-scripts/main/sync.sh | bash
