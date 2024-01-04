bfd0b30d74b79bc6687376a950f394ecb81a0e18
marat2509 <93652988+marat2509@users.noreply.github.com>
2024-01-04 22:51:22 +0300

    Create sync.sh

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+mkdir ~/OrangeFox_sync
+cd ~/OrangeFox_sync
+git clone https://gitlab.com/OrangeFox/sync.git
+cd ~/OrangeFox_sync/sync/
+./orangefox_sync.sh --branch 12.1 --path ~/fox_12.1
+cd fox_12.1
+echo "I: Synced to ~/fox_12.1"
