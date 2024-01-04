07d00a4997769651870f0b836fdc49aa88ffe89d
marat2509 <93652988+marat2509@users.noreply.github.com>
2024-01-04 22:59:58 +0300

    Update all.sh

--- all.sh
+++ all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo test
+export DEBIAN_FRONTEND=noninteractive
 sudo apt update
 sudo apt install curl -y
 curl -fSsl https://raw.githubusercontent.com/marat2509/OrangeFox-scripts/main/prepare.sh | bash
